82c66938e559cefde56664074893a1315e1f9c28
truon <htruong@foxeg.com>
2020-08-08 06:45:40 -0400

    Add img and architecture window folder

---
+++ src/architectureWindow/architectureWindow.js
@@ -0,0 +1,37 @@
+const ipcRenderer = require('electron').ipcRenderer;
+const mongoose = require('mongoose');
+const Architecture = require('../../Data/Models/architecture');
+require('dotenv/config');
+const DB_CONNECTION = "mongodb+srv://HT-Admin:Haotruong2@cluster0.fiqy6.mongodb.net/GeoArchitect?retryWrites=true&w=majority";
+
+async function getOneArchitecture(architectureName) {
+    mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, reconnectTries: 5000})
+    const table = await Architecture.find({name: architectureName}, function(err, data){
+      return data;  
+    })
+    mongoose.disconnect();
+    return table;
+}
+
+
+function getInfo() {
+    ipcRenderer.on('send-architectureName', async function(e, arg) {
+        var data = await getOneArchitecture(arg);
+
+        document.getElementsByClassName('architectName')[0].innerHTML = data[0].name;
+        document.getElementsByClassName('Location')[0].innerHTML = data[0].city;
+        document.getElementsByClassName('Author')[0].innerHTML = data[0].architects;
+        if (data[0].dateOpened){
+            document.getElementsByClassName('Date')[0].innerHTML = data[0].dateOpened;
+        }
+        if (data[0].info) {
+            document.getElementsByClassName('Information')[0].innerHTML = data[0].info;
+        }
+        if (data[0].picURL) {
+            document.getElementsByClassName('architectImage')[0].innerHTML = "<img src=\"../../assets/img/"+data[0].picURL+".jpg\">";
+        }
+    })
+    
+}
+
+
